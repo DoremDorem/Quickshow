fd2377978fff3b1a6742b31c6447bbed8dc3c982
DoremDorem <doremdorem2@gmail.com>
2025-08-30 08:49:05 +0530

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -27,13 +27,13 @@ const Navbar = () => {
             <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to='/'>Theaters</Link>
             <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to='/'>Release</Link>
             <Link onClick={()=>{scrollTo(0,0);setIsOpen(false)}} to='/favorite'>Favorites</Link>
+             <Link to='/admin'><button  className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full
+                 font-medium cursor-pointer
+              '>Dashboard</button></Link>
       </div>
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-         <Link to='/admin'><button  className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full
-                 font-medium cursor-pointer
-                 '>Dashboard</button></Link>
 
         {
             !user ?(
